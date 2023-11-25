4aa4f7d4cc9237f6014782bcdeea6e6ec85c2f43
Oleh Khoroshykh <oleg.khoroshikh@arbitas.com>
2023-11-25 19:59:47 +0200

    added handler addItem

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
-import walker from "./walker";
+import { readFile } from "fs/promises";
+import walker, { THandler } from "./walker";
 
-const report = require('../public/report.json');
-
-const logger = (value: any) => {
+const logger: THandler = (value) => {
   console.log(JSON.stringify(value, null, 2));
   return value;
 }
 
+interface ITransaction {
+  readonly "VSYMB": string[];
+  readonly "items"?: any[];
+}
+
 (async() => {
-  const obj = await walker(report, {'item': logger});
+
+  const addItems: THandler<ITransaction[]> = async (transactions) => {
+    try {
+      return transactions.reduce((acc, cur) => {
+        const [VSYMB] = cur.VSYMB;
+        const transaction: ITransaction = VSYMB === '00000603' ? { ...cur, items } : cur;
+        return [...acc, transaction];
+      }, [] as ITransaction[]);
+    } catch (error) {
+      console.error(error);
+      return transactions;
+    }
+  }
+
+  const path = process.cwd() + '/public';
+  const report = JSON.parse(await readFile(path + '/report.json', 'utf8'));
+  const items = JSON.parse(await readFile(path + '/items.json', 'utf8'));
+  const obj = await walker(report, {'transaction': [addItems, logger]});
 })();
--- src/walker.ts
+++ src/walker.ts
@@ -1,6 +1,19 @@
-export type THandler = <V = any>(value: V, key?: string, obj?: unknown, ...args: any[]) => Promise<V> | V;
+export type THandler<V = any> = (value: V) => Promise<V> | V;
+
 export interface IHandlers {
-  readonly [key: string]: THandler;
+  readonly [key: string]: THandler[];
+}
+
+export const runHandlers = async (value: any, handlers: THandler[]) => {
+  const [handler, ...rest] = handlers;
+  if (!handler) return value;
+
+  const _value = await runHandlers(
+    await handler(value),
+    rest,
+  );
+
+  return _value;
 }
 
 const walker = async (obj: unknown, handlers?: IHandlers) => {
@@ -8,8 +21,8 @@ const walker = async (obj: unknown, handlers?: IHandlers) => {
 
   await Promise.all(Object.entries(obj).map(async ([key, value]) => {
     try {
-      const handler = handlers && typeof handlers === 'object' && handlers[key];
-      const _value = handler ? await handler(value, key, obj) : value;
+      const keyHandlers = handlers && typeof handlers === 'object' && handlers[key];
+      const _value = keyHandlers ? await runHandlers(value, keyHandlers) : value;
 
       obj[key] = await walker(_value, handlers);
     } catch (error) {
